98fa01ef087817a3a51dfab19a89f172bb399199
Alabi Abayomi <abayomi15@gmail.com>
2020-11-25 13:42:45 +0100

    fixed logo import bug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-import { logo } from "../logo.svg";
+import logo from "../logo.svg";
 
 export default class Navbar extends Component {
     render() {

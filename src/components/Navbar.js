0b5e198b8d22f8d6d84f3581aca014b9439f8845
Alabi Abayomi <abayomi15@gmail.com>
2020-11-25 15:33:29 +0100

    Used styled-components for navbar and button.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import logo from "../logo.svg";
+import styled from "styled-components";
+import { ButtonContainer } from "./Button";
 
 export default class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
+            <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
                 <Link to="/">
-                    <img src={logo} alt="store" className="navbar-brand" />
+                 	<img src={logo} alt="store" className="navbar-brand" />
                 </Link>
                 <ul className="navbar-nav align-items-center">
                     <li className="navbar-item ml-4">
@@ -17,11 +19,23 @@ export default class Navbar extends Component {
                     </li>
                 </ul>
 								<Link to='/cart' className="ml-auto">
-									<button>
-										<i className="fas fa-cart-plus" />
-									</button>
+									<ButtonContainer>
+										<span className="mr-2">
+											<i className="fas fa-cart-plus" />
+										</span>
+										My cart
+									</ButtonContainer>
 								</Link>
-            </nav>
+            </NavWrapper>
         );
     }
 }
+
+const NavWrapper = styled.nav`
+		background: var(--mainBlue);
+		.nav-link{
+			color: var(--mainWhite)!important;
+			font-size: 1.3rem;
+			text-transform: capitalize;
+		}
+`

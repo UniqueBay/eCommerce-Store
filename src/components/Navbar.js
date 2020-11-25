7402f66e08568cf84f7909a45acb2aaa9a0a78bb
Alabi Abayomi <abayomi15@gmail.com>
2020-11-25 13:20:34 +0100

    Added navbar icons and links.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import { logo } from "../logo.svg";
 
 export default class Navbar extends Component {
     render() {
         return (
-            <div>
-                <h3>Hello from Navbar.</h3>
-            </div>
-        )
+            <nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
+                <Link to="/">
+                    <img src={logo} alt="store" className="navbar-brand" />
+                </Link>
+                <ul className="navbar-nav align-items-center">
+                    <li className="navbar-item ml-4">
+                        <Link to="/" className="nav-link">
+                            Items
+                        </Link>
+                    </li>
+                </ul>
+								<Link to='/cart' className="ml-auto">
+									<button>
+										<i className="fas fa-cart-plus" />
+									</button>
+								</Link>
+            </nav>
+        );
     }
 }

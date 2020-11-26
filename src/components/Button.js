43419407649e5c86603b7c06f8181f8b8d3c4abe
Alabi Abayomi <abayomi15@gmail.com>
2020-11-27 00:37:17 +0100

    spell check errors

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,17 @@
 import styled from 'styled-components'
 
 export const ButtonContainer = styled.button`
-		text-transfrom:capitalize;
+		text-transfrom: capitalize;
 		font-size: 1.4rem;
 		background: transparent;
 		border: 0.05rem solid var(--lightBlue);
-		border-color:${props => props.cart ? "var(--mainGreen)" : "var(--lightBlue)"};
-		color:${prop => prop.cart ? "var(--mainGreen)" : "var(--lightBlue)"};
+		border-color: ${props => 
+			props.cart ? "var(--mainGreen)" : "var(--lightBlue)"};
+		color: ${prop => (prop.cart ? "var(--mainGreen)" : "var(--lightBlue)")};
 		border-radius: 0.5rem;
 		padding: 0.2rem 0.5rem;
 		cursor: pointer;
-		margin: 0.2re, 0.5rem 0.2rem 0;
+		margin: 0.2rem 0.5rem 0.2rem 0;
 		transition: all 0.5s ease-in-out;
 	&:hover {
 		background: ${prop => prop.cart ? "var(--mainGreen)" : "var(--lightBlue)"};

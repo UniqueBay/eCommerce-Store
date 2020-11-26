af84191affbeacd63b5e2e966d17a11a8f6a86f0
Alabi Abayomi <abayomi15@gmail.com>
2020-11-26 23:57:52 +0100

    increment function in cart comp

--- src/context.js
+++ src/context.js
@@ -66,7 +66,23 @@ class ItemProvider extends Component {
 		});
 	};
 	increment = id => {
-		console.log('increment here');
+		let tempCart = [...this.state.cart];
+		const selectedProduct = tempCart.find(item => item.id === id);
+
+		const index = tempCart.indexOf(selectedProduct);
+		const product = tempCart[index];
+
+		product.count = product.count + 1;
+		product.total = product.count * product.price;
+
+		this.setState(
+			() => {
+				return { cart: [...tempCart] };
+			},
+			() => {
+				this.addTotals();
+			}
+		);
 	};
 	decrement = id => {
 		console.log('decrement here');

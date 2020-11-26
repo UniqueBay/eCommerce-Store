cda35fded3b070312adc3c0157630ea2e5e2ab1f
Alabi Abayomi <abayomi15@gmail.com>
2020-11-26 12:48:29 +0100

    fixed lingering issues.

--- src/context.js
+++ src/context.js
@@ -5,15 +5,28 @@ const ItemContext = React.createContext();
 
 class ItemProvider extends Component {
 	state ={
-		products: storeProducts,
+		products: [],
 		detailProduct: storeProducts 
+	};
+	componentDidMount(){
+		this.setProducts();
+	}
+	setProducts = () =>{
+		let tempProducts = [];
+		storeProducts.forEach(item =>{
+			const singleItem = {...item};
+			tempProducts = [...tempProducts,singleItem];
+		});
+		this.setState(() =>{
+			return{ products: tempProducts };
+		});
 	}
 	handleDetails = () =>{
 		console.log('hello from detail');
-	}
+	};
 	addToCart = () =>{
 		console.log('hello from add to cart');
-	}
+	};
 
     render() {
         return (

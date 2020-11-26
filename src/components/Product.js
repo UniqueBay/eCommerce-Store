5d0cf7a3cfef861982adcb1b3487583bafd93788
Alabi Abayomi <abayomi15@gmail.com>
2020-11-26 18:40:15 +0100

    Added modal components and functionality

--- src/components/Product.js
+++ src/components/Product.js
@@ -25,6 +25,7 @@ export default class Product extends Component {
 												disabled={inCart ? true : false} 
 												onClick={() => {
 													value.addToCart(id);
+													value.openModal(id);
 												}}
 												>
 												{inCart ? (
--- src/context.js
+++ src/context.js
@@ -6,7 +6,10 @@ const ItemContext = React.createContext();
 class ItemProvider extends Component {
 	state ={
 		products: [],
-		detailProduct: storeProducts 
+		detailProduct: storeProducts,
+		cart: [],
+		modalOpen: false,
+		modalProduct: storeProducts,
 	};
 	componentDidMount(){
 		this.setProducts();
@@ -34,15 +37,40 @@ class ItemProvider extends Component {
 		});
 	};
 	addToCart = id =>{
-		console.log(`hello from add to cart.id is ${id}`);
+		let tempProducts = [...this.state.products];
+		const index = tempProducts.indexOf(this.getItem(id));
+		const product = tempProducts[index];
+		product.inCart = true;
+		product.count = 1;
+		const price = product.price;
+		product.total = price;
+		this.setState(() => {
+			return { products: tempProducts, cart: [...this.state.cart, product] };
+		},
+		() => {
+			console.log(this.state)
+		});
 	};
+	openModal = id => {
+		const product = this.getItem(id);
+		this.setState(() => {
+			return {modalProduct: product, modalOpen: true}
+		})
+	}
+	closeModal = () => {
+		this.setState(() => {
+			return {modalOpen: false}
+		})
+	}
 
     render() {
         return (
             <ItemContext.Provider value={{
 														...this.state,
 														handleDetails: this.handleDetails,
-														addToCart: this.addToCart
+														addToCart: this.addToCart,
+														openModal: this.openModal,
+														closeModal: this.closeModal
 												}}
 												>
 													{this.props.children}

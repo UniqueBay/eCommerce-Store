b8690dfe779d2f89bb7d0c3f03ba7f3b5a6611a8
Alabi Abayomi <abayomi15@gmail.com>
2020-11-26 08:03:22 +0100

    Styled items, button and title for product views.

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -7,20 +7,19 @@ import { ItemConsumer } from "../context";
 export default class ProductList extends Component {
     
     render() {
-        console.log(this.state.products);
         return (
             <React.Fragment>
                 <div className="py-5">
                     <div className="container">
                         <Title name="our" title="Items" />
                         <div className="row">
-																									<ItemConsumer>
-																										{value => {
-																												return value.products.map( product =>{
-																													return <Product key={product.id} product={product} />;
-																												})
-																										}}
-																										</ItemConsumer>
+                            <ItemConsumer>
+                                {value => {
+                                        return value.products.map(product =>{
+                                            return <Product key={product.id} product={product} />;
+                                        })
+                                }}
+                            </ItemConsumer>
                         </div>
                     </div>
                 </div>

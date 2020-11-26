d536622664abfdf2e69f5a7ce26a8351901f1e6f
Alabi Abayomi <abayomi15@gmail.com>
2020-11-26 19:44:34 +0100

    Added EmptyCart component and function

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import Title from "./Title";
 import CartColums from "./CartColums";
+import EmptyCart from "./EmptyCart";
+import {ItemConsumer } from "../../context";
 
 export default class Cart extends Component {
     render() {
         return (
             <section>
-                <Title name="your" title='cart' />
-                <CartColums />
+                <ItemConsumer>
+                    {value =>{
+                        const {cart} = value;
+                        if(cart.lenght > 0){
+                            return (
+                                <React.Fragment>
+                                <Title name="your" title="cart" />
+                                <CartColums />
+                                </React.Fragment>
+                            );
+                        } else {
+                            return <EmptyCart />;
+                        }
+                    }}
+                </ItemConsumer>
             </section>
         );
     }
